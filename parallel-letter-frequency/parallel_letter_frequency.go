b23b57afe9f51c66c67da31b0eb1f6b73b28731b
Anton Voitovych <vojtoshik@gmail.com>
2020-11-12 21:12:23 +0100

    Solution for the Parallel Letter Frequency problem

---
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -0,0 +1,38 @@
+package letter
+
+// FreqMap records the frequency of each rune in a given text.
+type FreqMap map[rune]int
+
+func ConcurrentFrequency(texts []string) FreqMap {
+	c := make(chan FreqMap)
+	for _,text := range texts {
+		go goroutineFrequency(text, c)
+	}
+
+	subtotals := make(FreqMap)
+
+	for i := 0; i < len(texts); i++ {
+
+		var freqMap = <- c
+
+		for k,v := range freqMap {
+			subtotals[k] += v
+		}
+	}
+
+	return subtotals
+}
+
+func goroutineFrequency(s string, c chan <- FreqMap) {
+	c <- Frequency(s)
+}
+
+// Frequency counts the frequency of each rune in a given text and returns this
+// data as a FreqMap.
+func Frequency(s string) FreqMap {
+	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
